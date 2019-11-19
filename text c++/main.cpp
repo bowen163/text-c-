3f16e27faf31e0662db591fe64b9e44541929c64
bowen163 <763893371@qq.com>
2019-11-19 16:22:36 +0800

    Initial Commit

---
+++ text c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  text c++
+//
+//  Created by s20181102936 on 2019/11/19.
+//  Copyright © 2019 s20181102936. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
